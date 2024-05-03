70ae9abc98458ff9f4dfafd8cb2bc3cfaa7280fd
oluwatimilehin awoniyi <awoniyitimi@outlook.com>
2024-05-03 03:42:34 +0000

    making changes

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Given a pile of coins of different values,
+    determine the fewest number of coins needed
+    to meet a given amount total.
+"""
+
+
+def makeChange(coins, total):
+    if total <= 0:
+        return 0
+
+    dp = [float('inf')] * (total + 1)
+    dp[0] = 0
+
+    for coin in coins:
+        for amount in range(coin, total + 1):
+            dp[amount] = min(dp[amount], dp[amount - coin] + 1)
+
+    if dp[total] == float('inf'):
+        return -1
+    else:
+        return dp[total]
